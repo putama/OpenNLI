0c0d7f3a30cbb1d186b9127ca7610de3cf48daf6
Ajie Utama <prutama@Ajies-MacBook-Pro.local>
2018-09-24 11:56:59 +0200

    set bucketting as default

--- datautil.py
+++ datautil.py
@@ -1,6 +1,7 @@
 from torchtext import data, datasets
 import torch
 
+
 def build_mnli_split(arguments, reverse=True):
     text_field = datasets.nli.ParsedTextField(reverse=reverse)
     label_field = data.LabelField()
@@ -16,8 +17,8 @@ def build_mnli_split(arguments, reverse=True):
                                       validation="multinli_1.0_dev_matched.jsonl",
                                       test="multinli_1.0_dev_mismatched.jsonl")
     train_split, dev_m_split, dev_um_split = splits
-    splits = datasets.MultiNLI.splits(text_field, label_field, parse_field, 
-                                      genre_field, pairID_field, 
+    splits = datasets.MultiNLI.splits(text_field, label_field, parse_field,
+                                      genre_field, pairID_field,
                                       root=arguments.data_root,
                                       train=None,
                                       validation="multinli_0.9_test_"
@@ -41,42 +42,54 @@ def build_mnli_split(arguments, reverse=True):
     return train_split, dev_m_split, test_m_split, dev_um_split, test_um_split
 
 
-def build_nli_iterator(split, args, training=True):
+def custom_sort_key(ex):
+    return max(len(ex.premise), len(ex.hypothesis))
+
+
+def build_nli_iterator(split, args, training=True, bucket=True):
     """
     :param split: Dataset
     :param args: arguments
     :param training: is training or not
     :return: iterator of dataset
     """
+    print("build nli iterator for the specified sets...")
+    iteratorCls = data.BucketIterator if bucket else data.Iterator
 
     if training:
-        data_iters = data.Iterator.splits((split,), batch_size=(args.batch_size),
-                                          repeat=False)
+        data_iters = iteratorCls.splits((split,), batch_size=(args.batch_size),
+                                        repeat=False, sort_within_batch=True,
+                                        sort_key=custom_sort_key)
     else:
-        data_iters = data.Iterator.splits((split,), shuffle=False,
-                                          batch_size=args.batch_size,
-                                          repeat=False)
+        data_iters = iteratorCls.splits((split,), shuffle=False,
+                                        batch_size=args.batch_size, repeat=False,
+                                        sort_within_batch=True, sort_key=custom_sort_key
+                                        )
     return data_iters[0]
 
 
-def build_nli_iterator_all(splits, args):
+def build_nli_iterator_all(splits, args, bucket=True):
+    print("build nli iterator for all sets...")
     if args.nli_dataset == "multinli":
         train, dev_match, test_match, dev_umatch, test_umatch = splits
     else:
         raise Exception("invalid NLI dataset name")
 
-    (multinli_train_iter,) = data.Iterator.splits((train,),
-                                                  batch_size=(args.batch_size),
-                                                  repeat=False)
+    iteratorCls = data.BucketIterator if bucket else data.Iterator
 
-    match_iters = data.Iterator.splits((dev_match, test_match), shuffle=False,
-                                       batch_size=args.batch_size_eval,
-                                       repeat=False)
+    (multinli_train_iter,) = iteratorCls.splits((train,),
+                                                batch_size=(args.batch_size),
+                                                sort_within_batch=True,
+                                                repeat=False, sort_key=custom_sort_key)
+
+    match_iters = iteratorCls.splits((dev_match, test_match), shuffle=False,
+                                     batch_size=args.batch_size_eval, repeat=False,
+                                     sort_within_batch=True, sort_key=custom_sort_key)
     multinli_dev_match_iter, multinli_test_match_iter = match_iters
 
-    umatch_iters = data.Iterator.splits((dev_umatch, test_umatch), shuffle=False,
-                                        batch_size=args.batch_size_eval,
-                                        repeat=False)
+    umatch_iters = iteratorCls.splits((dev_umatch, test_umatch), shuffle=False,
+                                      batch_size=args.batch_size_eval, repeat=False,
+                                      sort_within_batch=True, sort_key=custom_sort_key)
     multinli_dev_umatch_iter, multinli_test_umatch_iter = umatch_iters
 
     iters = (multinli_train_iter, multinli_dev_match_iter, multinli_dev_umatch_iter,
